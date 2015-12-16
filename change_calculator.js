a62822c86727ab2fa401e29a28e926b20d3e1bae
aaronwbrown <aaron.w.brown13@gmail.com>
2015-12-16 14:23:29 -0800

    Update change_calculator.js

--- change_calculator.js
+++ change_calculator.js
@@ -39,48 +39,54 @@ Challenge Input
 00.06
 */
 
-var myNum = 45.67;
+//This is super sloppy, but it works.
 
-var decimalToWhole = function(num) { // function to change a decimal into a whole number
-    var inputNum = (num); // working variable
-    var numCeil = Math.ceil(inputNum); // take top of the whole number
-    var numDiff = numCeil - inputNum; // difference between top whole and inputNum
-    var numDecimal = 1 - numDiff; // find decimal amount
-    var numWhole = Math.floor(numDecimal * 100); // convert to whole number and take bottom (long decimal sensitivity not a factor)
-    return numWhole; // return value
+var myNum = .99;
+var numBottom = Math.floor(myNum);
+
+var getRemainderAll = function(num) {
+    return Math.round((myNum%.25) * 100);
 };
-var numQuarters = function(num) { // find the amount of quarters in the input number
-    console.log("Quarters: " + Math.floor(num) / .25);
+
+var numberOfDimes = function(num) { // find the amount of dimes in the input number
+    return Math.floor(getRemainderAll(num) / 10);
 };
-var numberOfDimes = function(num) { // find the amoung of dimes in the input number
-    return Math.floor(num / 10);
+
+var remainderAfterDimes = function(num) {
+    return ((numberOfDimes(num)*10) - getRemainderAll(num))*-1;
 };
-var remainder = function(num) { // find the remainder after taking out the dimes
-    return (numberOfDimes(num) * 10 - num) * -1;
+
+var getQuarters = function(num) {
+    var quarterRemainder = (num / .25) - (numBottom / .25);  
+    var finalQuarters = Math.floor(quarterRemainder) + (numBottom/.25);
+    return finalQuarters;
 };
-var newFunc = function(num) { // function to determine the smallest amount of change (not including quarters) needed
-    var numToWhole = decimalToWhole(num) // converting input number into our numWhole format
-    console.log("Dimes: " + numberOfDimes(numToWhole)); // taking input and finding the number of dimes needed
-    if (remainder(numToWhole) > 5) { // finding if nickels are needed
+var getChange = function(num) {
+  var remainderChange = getRemainderAll(num);
+  var numDimes = numberOfDimes(num);
+  console.log("Dimes " + numDimes);
+  var remainder = (numDimes * 10 - remainderChange) * -1;
+  if (remainder > 5) { // finding if nickels are needed
         console.log("Nickels: " + 1); // if they are, just add 1 to the string. you will never need more than 1 nickel when finding the lowest amount of coins
-        console.log("Pennies: " + (remainder(numToWhole) - 5) * 1); // taking remainder for pennies
+        console.log("Pennies: " + Math.round((remainder - 5) * 1)); // taking remainder for pennies
     } else {
         console.log("Nickels: " + 0) // nickels are not needed
-        console.log("Pennies: " + (remainder(numToWhole) * 1)); // taking remainder for pennies
+        console.log("Pennies: " + Math.round((remainder * 1))); // taking remainder for pennies
     }
 };
+
 var changeCalc = function(num) {
-    numQuarters(num);
-    newFunc(num);
+    console.log("Quarters: " + getQuarters(num));
+    console.log(getChange(num));
 };
 
 changeCalc(myNum);
 
 /*
-//Input - 45.67
+//Input - .99
 //OUTPUT
-Quarters: 180
-Dimes: 6
-Nickels: 1
-Pennies: 2
+Quarters: 3
+Dimes: 2
+Nickels: 0
+Pennies: 4
 */
